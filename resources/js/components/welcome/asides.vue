197fdb445f137825460e9308a7b036c0159f961d
Vongkeo <vongkeokeosavanh@gmail.com>
2019-05-03 09:58:09 +0700

    add stock name

--- resources/js/components/welcome/asides.vue
+++ resources/js/components/welcome/asides.vue
@@ -22,6 +22,61 @@
               <p class="text-aside">首页</p>
             </a>
           </li>
+           <li>
+            <a data-toggle="collapse" href="#pagesExamples">
+              <i class="tim-icons icon-image-02"></i>
+              <p>
+                Pages
+                <b class="caret"></b>
+              </p>
+            </a>
+            <div class="collapse" id="pagesExamples">
+              <ul class="nav">
+                <li>
+                  <a href="../examples/pages/pricing.html">
+                    <span class="sidebar-mini-icon">P</span>
+                    <span class="sidebar-normal"> Pricing </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/pages/rtl.html">
+                    <span class="sidebar-mini-icon">RS</span>
+                    <span class="sidebar-normal"> RTL Support </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/pages/timeline.html">
+                    <span class="sidebar-mini-icon">T</span>
+                    <span class="sidebar-normal"> Timeline </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/pages/login.html">
+                    <span class="sidebar-mini-icon">L</span>
+                    <span class="sidebar-normal"> Login </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/pages/register.html">
+                    <span class="sidebar-mini-icon">R</span>
+                    <span class="sidebar-normal"> Register </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/pages/lock.html">
+                    <span class="sidebar-mini-icon">LS</span>
+                    <span class="sidebar-normal"> Lock Screen </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/pages/user.html">
+                    <span class="sidebar-mini-icon">UP</span>
+                    <span class="sidebar-normal"> User Profile </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
           <li class="nav-aside">
             <a href="examples/dashboard.html">
               <i class="tim-icons icon-chart-pie-36"></i>
