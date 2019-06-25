3577b0b12533638b7a5a528dcb7aa66e97cb8fbc
bankonly <47017349+bankonly@users.noreply.github.com>
2019-06-25 15:46:01 +0700

    pull

--- resources/js/components/lobby.vue
+++ resources/js/components/lobby.vue
@@ -260,7 +260,7 @@ export default {
       };
       axios
         .post(
-          "http://localhost:8003/checkapiuser",
+          // "http://localhost:8003/checkapiuser",
           {
             id: this.id,
             user_id: this.user_id,
@@ -296,8 +296,8 @@ export default {
           this.firstname = res.data[0].id;
           this.lastname = res.data[0].secret;
           let filename = window.location.href
-            // this.gamelink = `http://lec68.com/redirect?&name=${this.email}&urlback=http://${filename.split('/')[2]}`
-            this.gamelink = `http://localhost:8003/redirect?&name=${this.email}&urlback=http://${filename.split('/')[2]}`
+            this.gamelink = `http://lec68.com/redirect?&name=${this.email}&urlback=http://${filename.split('/')[2]}`
+            // this.gamelink = `http://localhost:8003/redirect?&name=${this.email}&urlback=http://${filename.split('/')[2]}`
           // $('.id').val(this.id);
           // $('.provider_name').val(this.provider_name);
           // $('.balance').val(this.balance);
