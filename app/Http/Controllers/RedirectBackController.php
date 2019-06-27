7d37076ee8a1422498a29486b8483a909c757b27
bankonly <47017349+bankonly@users.noreply.github.com>
2019-06-27 11:39:37 +0700

    pull

---
+++ app/Http/Controllers/RedirectBackController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use App\access_token;
+class RedirectBackController extends Controller
+{
+    public function redirect(){
+        $http = new Client();
+
+        $query = http_build_query([
+            'client_id' => '3',
+            'redirect_uri' => 'http://www.ecg9.com/callback',
+            'response_type' => 'code',
+            'scope' => '',
+        ]);
+        return redirect('http://lec68.com/oauth/authorize?'.$query);
+    }
+    public function callback(Request $request){
+        $http = new Client;
+            $response = $http->post('http://lec68.com/oauth/token', [
+                'form_params' => [
+                    'grant_type' => 'authorization_code',
+                    'client_id' => '3',
+                    'client_secret' => 'XhW9xjDWoh3ieRwmECmSm82iDJ7Gv2OOkGi6HU2W',
+                    'redirect_uri' => 'http://www.ecg9.com/callback',
+                    'code' => $request->code,
+                ],
+            ]);
+            $accessdata = json_decode((string) $response->getBody(), true);
+            $header =[
+                'Content-Type' => 'application/json',
+                'Accept'=>'application/json',
+                'Authorization'=>'Bearer '.$accessdata['access_token']
+            ];
+            $resuser = $http->get('http://lec68.com/api/users',['headers'=>$header]);
+            $data =  json_decode((string) $resuser->getBody(), true);
+            $date = date('Y-m-d');
+            $check = access_token::where([['created_at','like','%'.$date.'%'],['user_id','=',''.$data['user_id'].'']])->get()->count();
+            if($check < 1){
+                access_token::create([
+                    'user_id'=>$data['user_id'],
+                    'access_token'=>$accessdata['access_token']
+                ]);
+            }
+            return redirect('http://lec68.com/igotologin');
+        //     // return [$accessdata,$data];
+        
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -129,93 +129,60 @@ Route::get('admin/check', function () {
 
 Route::post('/checklogin', 'MasterController@checklogin');
 Route::get('/redirectback', function () {
-    // asdasd
-    $id = \Auth::user()->pro_id . '_' . \Auth::user()->user_id;
-    $http = new GuzzleHttp\Client();
-    // function dehash(){
-    $data = \Auth::user()->pwdhashed;
-    $pwd = explode('-', $data);
-    $gotpwd = [];
-    foreach ($pwd as $p) {
-        $gotpwd[] = substr($p, -1, 1);
-    }
-    $realpwd = implode('', $gotpwd);
-    $dehashed =  $realpwd; //GOTED PASSWORD
-
-    // }
-    // return $dehashed;
-
-    $response = $http->post('http://lec68.com/oauth/token', [
-        'form_params' => [
-            'grant_type' => 'password',
-            'client_id' => '2',
-            'client_secret' => 'n7ZrJ7VGv4b6QuQjZ1AKWZ4w4AuvX88JuxzlPjGu',
-            'username' => \Auth::user()->email,
-            'password' => $dehashed,
-            'scope' => '',
-        ],
+    $query = http_build_query([
+        'client_id' => 'client-id',
+        'redirect_uri' => 'http://localhost:8004/callback',
+        'response_type' => 'code',
+        'scope' => '',
     ]);
-    $accessdata = json_decode((string)$response->getBody(), true);
-    $header = [
-        'Content-Type' => 'application/json',
-        'Accept' => 'application/json',
-        'Authorization' => 'Bearer ' . $accessdata['access_token']
-    ];
-    $resuser = $http->get('http://lec68.com/api/users', ['headers' => $header]);
-    $data =  json_decode((string)$resuser->getBody(), true);
-    $date = date('Y-m-d');
-    $check = access_token::where([['created_at', 'like', '%' . $date . '%'], ['user_id', '=', '' . $data['user_id'] . '']])->get()->count();
-    if ($check < 1) {
-        access_token::create([
-            'user_id' => $data['user_id'],
-            'access_token' => $accessdata['access_token']
-        ]);
-    }
-    return redirect('http://lec68.com/igotologin');
+    return redirect('http://localhost:8003/oauth/authorize?'.$query);
+    // // asdasd
+    // $id = \Auth::user()->pro_id . '_' . \Auth::user()->user_id;
+    // $http = new GuzzleHttp\Client();
+    // // function dehash(){
+    // $data = \Auth::user()->pwdhashed;
+    // $pwd = explode('-', $data);
+    // $gotpwd = [];
+    // foreach ($pwd as $p) {
+    //     $gotpwd[] = substr($p, -1, 1);
+    // }
+    // $realpwd = implode('', $gotpwd);
+    // $dehashed =  $realpwd; //GOTED PASSWORD
+
+    // // }
+    // // return $dehashed;
+
+    // $response = $http->post('http://localhost:8003/oauth/token', [
+    //     'form_params' => [
+    //         'grant_type' => 'password',
+    //         'client_id' => '2',
+    //         'client_secret' => 'n7ZrJ7VGv4b6QuQjZ1AKWZ4w4AuvX88JuxzlPjGu',
+    //         'username' => \Auth::user()->email,
+    //         'password' => $dehashed,
+    //         'scope' => '',
+    //     ],
+    // ]);
+    // $accessdata = json_decode((string)$response->getBody(), true);
+    // $header = [
+    //     'Content-Type' => 'application/json',
+    //     'Accept' => 'application/json',
+    //     'Authorization' => 'Bearer ' . $accessdata['access_token']
+    // ];
+    // $resuser = $http->get('http://localhost:8003/api/users', ['headers' => $header]);
+    // $data =  json_decode((string)$resuser->getBody(), true);
+    // $date = date('Y-m-d');
+    // $check = access_token::where([['created_at', 'like', '%' . $date . '%'], ['user_id', '=', '' . $data['user_id'] . '']])->get()->count();
+    // if ($check < 1) {
+    //     access_token::create([
+    //         'user_id' => $data['user_id'],
+    //         'access_token' => $accessdata['access_token']
+    //     ]);
+    // }
+    // return redirect('http://localhost:8003/igotologin');
 });
-// Route::get('redirect',function(){
-//     $http = new GuzzleHttp\Client();
+Route::get('/redirect', 'RedirectBackController@redirect');
+Route::get('/callback', 'RedirectBackController@callback');
 
-//     // $query = http_build_query([
-//     //     'client_id' => '3',
-//     //     'redirect_uri' => 'http://localhost:8004/callback',
-//     //     'response_type' => 'code',
-//     //     'scope' => '',
-//     // ]);
-//     //  return redirect('http://localhost:8003/oauth/authorize?'.$query);
-// })->name('get.token');
-// Route::get('/callback', function (Request $request) {
-
-//     $http = new GuzzleHttp\Client;
-//     $response = $http->post('http://localhost:8003/oauth/token', [
-//         'form_params' => [
-//             'grant_type' => 'authorization_code',
-//             'client_id' => '3',
-//             'client_secret' => 'sBR3DGz8O9RJLkDa1Q4rixdC19iWktnjDca3yvBV',
-//             'redirect_uri' => 'http://localhost:8004/callback',
-//             'code' => $request->code,
-//         ],
-//     ]);
-//     $accessdata = json_decode((string) $response->getBody(), true);
-//     $header =[
-//         'Content-Type' => 'application/json',
-//         'Accept'=>'application/json',
-//         'Authorization'=>'Bearer '.$accessdata['access_token']
-//     ];
-//     $resuser = $http->get('http://localhost:8003/api/users',['headers'=>$header]);
-//     $data =  json_decode((string) $resuser->getBody(), true);
-//     $date = date('Y-m-d');
-//     $check = access_token::where([['created_at','like','%'.$date.'%'],['user_id','=',''.$data['user_id'].'']])->get()->count();
-//     if($check < 1){
-//         access_token::create([
-//             'user_id'=>$data['user_id'],
-//             'access_token'=>$accessdata['access_token']
-//         ]);
-//     }
-//     return redirect('http://localhost:8003/getlogin');
-// //     // return [$accessdata,$data];
-
-// });
 
 /////timer
 Route::get('locale/{locale}', function ($locale) {
