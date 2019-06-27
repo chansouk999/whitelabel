db812cd77b972551c651d0c1493bc4ebfb90d4e2
bankonly <47017349+bankonly@users.noreply.github.com>
2019-06-27 10:54:44 +0700

    pill

--- routes/web.php
+++ routes/web.php
@@ -129,6 +129,7 @@ Route::get('admin/check', function () {
 
 Route::post('/checklogin', 'MasterController@checklogin');
 Route::get('/redirectback', function () {
+    // asdasd
     $id = \Auth::user()->pro_id . '_' . \Auth::user()->user_id;
     $http = new GuzzleHttp\Client();
     // function dehash(){
@@ -144,11 +145,11 @@ Route::get('/redirectback', function () {
     // }
     // return $dehashed;
 
-    $response = $http->post('http://localhost:8003/oauth/token', [
+    $response = $http->post('http://lec68.com/oauth/token', [
         'form_params' => [
             'grant_type' => 'password',
             'client_id' => '2',
-            'client_secret' => 'A0Y369gN4BxWPFxcUyjyZWkmFM6dgqBNxdtHYX8g',
+            'client_secret' => 'n7ZrJ7VGv4b6QuQjZ1AKWZ4w4AuvX88JuxzlPjGu',
             'username' => \Auth::user()->email,
             'password' => $dehashed,
             'scope' => '',
@@ -160,7 +161,7 @@ Route::get('/redirectback', function () {
         'Accept' => 'application/json',
         'Authorization' => 'Bearer ' . $accessdata['access_token']
     ];
-    $resuser = $http->get('http://localhost:8003/api/users', ['headers' => $header]);
+    $resuser = $http->get('http://lec68.com/api/users', ['headers' => $header]);
     $data =  json_decode((string)$resuser->getBody(), true);
     $date = date('Y-m-d');
     $check = access_token::where([['created_at', 'like', '%' . $date . '%'], ['user_id', '=', '' . $data['user_id'] . '']])->get()->count();
@@ -170,7 +171,7 @@ Route::get('/redirectback', function () {
             'access_token' => $accessdata['access_token']
         ]);
     }
-    return redirect('http://localhost:8003/igotologin');
+    return redirect('http://lec68.com/igotologin');
 });
 // Route::get('redirect',function(){
 //     $http = new GuzzleHttp\Client();
