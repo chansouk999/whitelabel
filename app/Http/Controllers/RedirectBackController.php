cd85f32342205f5e727f39e5e8d58be9a2fd1c1c
bankonly <47017349+bankonly@users.noreply.github.com>
2019-07-03 08:32:43 +0700

    pull

--- app/Http/Controllers/RedirectBackController.php
+++ app/Http/Controllers/RedirectBackController.php
@@ -6,33 +6,53 @@ use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use App\access_token;
 use Illuminate\Support\Facades\Cache;
+use App\OauthClient;
 
 class RedirectBackController extends Controller
 {
+    public function getsecret($id, $url)
+    {
+        try {
+            // clientid
+            $http =  new Client;
+            $res = $http->post($url . '/returnrequest', [
+                'form_params' => [
+                    'clientid' => $id,
+                ]
+            ]);
+            $reqdata = json_decode((string) $res->getBody(), true);
+            Cache::put('MainClientID', $id, 3147502);
+            Cache::put('MainClientSecret', $reqdata['secret'], 3147502);
+        } catch (\Exception $ex) {
+            return $ex->getMessage();
+        }
+    }
     public function redirect(Request $req)
     {
-        Cache::put('mainUrl',$req->url,3714526);
+        // return $req;
+        $this->getsecret($req->CurrentClientID, $req->url);
+        Cache::put('mainUrl', $req->url, 3714526);
         // Cache::get('mainUrl');
         $http = new Client();
 
         $query = http_build_query([
-            'client_id' => '3',
-            'redirect_uri' => \Request::root().'/callback',
+            'client_id' => $req->CurrentClientID,
+            'redirect_uri' => \Request::root() . '/callback',
             'response_type' => 'code',
             'scope' => '',
         ]);
         // Cache::get('mainUrl');
-        return redirect(Cache::get('mainUrl').'/oauth/authorize?' . $query);
+        return redirect(Cache::get('mainUrl') . '/oauth/authorize?' . $query);
     }
     public function callback(Request $request)
     {
         $http = new Client;
-        $response = $http->post( Cache::get('mainUrl').'/oauth/token', [
+        $response = $http->post(Cache::get('mainUrl') . '/oauth/token', [
             'form_params' => [
                 'grant_type' => 'authorization_code',
-                'client_id' => '3',
-                'client_secret' => 'XhW9xjDWoh3ieRwmECmSm82iDJ7Gv2OOkGi6HU2W',
-                'redirect_uri' => \Request::root().'/callback',
+                'client_id' => Cache::get('MainClientID'),
+                'client_secret' => Cache::get('MainClientSecret'),
+                'redirect_uri' => \Request::root() . '/callback',
                 'code' => $request->code,
             ],
         ]);
@@ -42,9 +62,9 @@ class RedirectBackController extends Controller
             'Accept' => 'application/json',
             'Authorization' => 'Bearer ' . $accessdata['access_token']
         ];
-        Cache::put('webToken',$accessdata['access_token'],3714526);
+        Cache::put('webToken', $accessdata['access_token'], 3714526);
 
-        $resuser = $http->get(Cache::get('mainUrl').'/api/users', ['headers' => $header]);
+        $resuser = $http->get(Cache::get('mainUrl') . '/api/users', ['headers' => $header]);
         $data =  json_decode((string) $resuser->getBody(), true);
         $date = date('Y-m-d');
         $check = access_token::where([['created_at', 'like', '%' . $date . '%'], ['user_id', '=', '' . $data['user_id'] . '']])->get()->count();
@@ -55,7 +75,7 @@ class RedirectBackController extends Controller
             ]);
         }
 
-        return redirect(Cache::get('mainUrl').'/igotologin');
+        return redirect(Cache::get('mainUrl') . '/igotologin');
         //     // return [$accessdata,$data];
 
     }
