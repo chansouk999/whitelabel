c042feab0c684a57c80ea7a682179a95d01d5db9
bankonly <47017349+bankonly@users.noreply.github.com>
2019-07-06 10:14:20 +0700

    pull

--- app/Announcement.php
+++ app/Announcement.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Announcement extends Model
 {
-    //
+    protected $fillable = [
+        'AnouncementID',
+        'method',
+        'message',
+        'addInDate'
+    ];
 }
