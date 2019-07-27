70bbb91fa064aa3649994f435ed93e61dabbc143
bbmek999 <bbmek999@users.noreply.github.com>
2019-07-27 15:54:26 +0700

    go go not yet

--- resources/js/app.js
+++ resources/js/app.js
@@ -58,4 +58,60 @@ Vue.use(VueAwesomeSwiper)
 
 const app = new Vue({
     el: '#app',
+    data: {
+        // messages: [],
+        // users: [],
+    },
+
+    created() {
+        // this.fetchMessages();
+
+        // Echo.join('chat')
+        //     .here(users => {
+        //         this.users = users;
+        //     })
+        //     .joining(user => {
+        //         this.users.push(user);
+        //     })
+        //     .leaving(user => {
+        //         this.users = this.users.filter(u => u.id !== user.id);
+        //     })
+        //     .listenForWhisper('typing', ({ id, name }) => {
+        //         this.users.forEach((user, index) => {
+        //             if (user.id === id) {
+        //                 user.typing = true;
+        //                 this.$set(this.users, index, user);
+        //             }
+        //         });
+        //     })
+        //     .listen('MessageSent', (event) => {
+        //         this.messages.push({
+        //             message: event.message.message,
+        //             user: event.user
+        //         });
+
+        //         this.users.forEach((user, index) => {
+        //             if (user.id === event.user.id) {
+        //                 user.typing = false;
+        //                 this.$set(this.users, index, user);
+        //             }
+        //         });
+        //     });
+    },
+
+    methods: {
+        //     fetchMessages() {
+        //         axios.get('/messages').then(response => {
+        //             this.messages = response.data;
+        //         });
+        //     },
+
+        //     addMessage(message) {
+        //         this.messages.push(message);
+
+        //         axios.post('/messages', message).then(response => {
+        //             console.log(response.data);
+        //         });
+        //     }
+    }
 })
