cb8df2ab1fe15c6794176ae4cd38294ba97685ec
bbmek999 <bbmek999@users.noreply.github.com>
2019-07-29 15:56:46 +0700

    websockets not yet

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -51,6 +51,7 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
-    wsPort: 8002,
+    wsPort: 6001,
     disableStats: true,
+    encrypted: false,
 });
