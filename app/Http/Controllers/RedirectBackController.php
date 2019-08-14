55563698d13d2252fb92541a9f6702a1f5ddcfb8
bankonly <47017349+bankonly@users.noreply.github.com>
2019-08-14 21:29:39 +0700

    pykk

--- app/Http/Controllers/RedirectBackController.php
+++ app/Http/Controllers/RedirectBackController.php
@@ -50,11 +50,7 @@ class RedirectBackController extends Controller
         // Cache::get('mainUrl');
         return redirect(Cache::get('mainUrl') . '/oauth/authorize?' . $query);
     }
-    public function Getlogin()
-    {
-        Auth::loginUsingId(Cache::get('userid'));
-        return redirect(Cache::get('mainUrl') . '/getlogin?stockname=' . Cache::get("name") . '&loop=' . Cache::get("loop") . '&country=' . Cache::get("country"));
-    }
+  
     public function callback(Request $request)
     {
         $http = new Client;
